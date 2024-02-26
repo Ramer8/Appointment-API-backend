b148fca943f77fa03157a799b3ec301486ba0c88
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-02-26 23:51:29 +0100

    connected database & added keys to .env

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,21 @@
-import 'dotenv/config'
-import { app } from './app'
+import "dotenv/config"
+import { app } from "./app"
+import { AppDataSource } from "./database/db"
 
 const PORT = process.env.PORT || 4500
 
-
-const startServer= ()=> {
-
-    app.listen(PORT,()=>{
-        console.log(`Server is running at PORT: ${PORT}`);
+const startServer = () => {
+    
+  AppDataSource.initialize()
+    .then(() => {
+      console.log("Database Appointment API connected")
+      app.listen(PORT, () => {
+        console.log(`Server is running at PORT: ${PORT}`)
+      })
+    })
+    .catch((error) => {
+      console.log(error)
     })
 }
 
-startServer();
+startServer()
