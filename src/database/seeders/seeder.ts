774c5979f462cd1afda1a43a093deb1de4822011
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-03-26 09:00:35 +0100

    Update: text in seeders file

--- src/database/seeders/seeder.ts
+++ src/database/seeders/seeder.ts
@@ -101,34 +101,35 @@ const serviceSeedDatabase = async () => {
     const service1 = new Service()
     service1.serviceName = "Custom tatoo"
     service1.description =
-      "Customer can bring his design and we ink it on his body"
+      "Customers will have the freedom to select unique motifs and designs, fully customizingtheir tattoo experience according to their preferences and tastes."
     service1.id = 1
     await service1.save()
 
     const service2 = new Service()
     service2.serviceName = "Web catalog tatoo"
     service2.description =
-      "We have severals desingn on predefined designs in our catalog."
+      "We offer the realization of tattoos based on predefined designs in our catalog.catalog. Customers can choose from a variety of stylish and proven options."
     service2.id = 2
     await service2.save()
 
     const service3 = new Service()
     service3.serviceName = "Old tattoo restoration"
-    service3.description = "We can fix old blur tattos"
+    service3.description =
+      "We specialize in the restoration and rejuvenation of existing tattoos. Ourexperts work to enhance and renew old tattoos, giving them back their vitality."
     service3.id = 3
     await service3.save()
 
     const service4 = new Service()
     service4.serviceName = "Piercing and dilator insertion"
     service4.description =
-      "We offer professional services for piercing and dilator placement"
+      "We offer professional services for the placement of piercings and dilators. OurOur team guarantees safe procedures and varied styles to meet the individual preferences of our clients.preferences of our client"
     service4.id = 4
     await service4.save()
 
     const service5 = new Service()
     service5.serviceName = "Sale of piercings and other articles"
     service5.description =
-      "In addition to our application services, we offer a selection of piercings and other body art related items. Customers can purchase quality products to complement their unique style."
+      "In addition to our services, we offer a selection of piercings and other body art related items. Customers can purchase quality products to complement their unique style."
     service5.id = 5
     await service5.save()
     console.log("----------------------------")
